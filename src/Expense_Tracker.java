56ce904145a0c2afdae172286cdc19f2fe4208eb
CodeWithGoodness <ceciliaOkoro2@gmail.com>
2022-04-23 15:33:35 -0700

    first commit

--- src/Expense_Tracker.java
+++ src/Expense_Tracker.java
@@ -7,7 +7,7 @@ public class Expense_Tracker {
     System.out.println("EXPENSE TRACKER" );
        while (true) {
            System.out.println("What would you like to do? " +
-                   "\n 1.Add an Expense \n 2.Check list of Expenses \n 3.Edit an Expense \n 4.Exit");
+                   "\n 1.Add an Expense \n 2.Check list of Expenses \n 3. Delete an Expense \n 4.Exit");
           String option = input.next();
           exp.Options(option);
        }
@@ -15,26 +15,43 @@ public class Expense_Tracker {
 }
 
 class Methods {
-    private ArrayList<String> listArray = new ArrayList<>();
+    private ArrayList<String> listTitle = new ArrayList<>();
+    private ArrayList<String> listAmount = new ArrayList<>();
+    private ArrayList<String> listTag = new ArrayList<>();
+    private ArrayList<ArrayList<String>> collection = new ArrayList();
     public Methods(String listTitle, double amount, String tags) {
     }
     public void Option1 (){
         Scanner input = new Scanner(System.in);
+        System.out.println("How many expenses do you want to add?");
+        int number = input.nextInt();
         System.out.println("Enter in this order: title, amount, tag ");
-        for (int row = 0; row < listArray.size(); row++) {
-                listArray.add(input.next());
-
+        for (int i = 1; i <= number; i++) {
+            listTitle.add(input.next());
+            listAmount.add(input.next());
+            listTag.add(input.next());
+            collection.add(listTitle);
+            collection.add(listAmount);
+            collection.add(listTag);
         }
     }
     public void Option2(){
-        if (listArray .isEmpty()){
+        if (collection.isEmpty()){
             System.out.println("Your expense list is empty");}
         else {
             System.out.println("Your expenses are as follows: ");
-            for (int row = 0; row < listArray.size(); row++) {
-                    System.out.print(listArray.get(row) + " ");
+            for (int i = 0; i < collection.get(1).size(); i++) {
+                    System.out.print( collection.get(0).get(i) + ": " + collection.get(1).get(i) +"(" + collection.get(2).get(i)+ ")" );
+                    System.out.println();
             }
         }
+    }
+    public void Option3(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input expense to delete");
+        String search = input.next();
+        if ( listTitle.contains(search))
+            listTitle.remove(search);
     }
     public  void Options (String option){
         if (option.equals("1"))
@@ -46,6 +63,4 @@ class Methods {
         else
             System.out.println("error");
     }
-
-
 }
